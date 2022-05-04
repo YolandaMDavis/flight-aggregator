c72d61a83f01da4a422cb7173ef3736395963c0f
Yolanda M. Davis <yolanda.m.davis@gmail.com>
2022-05-04 15:22:49 -0400

    bug fix to summary stream

--- src/main/scala/com/flight/aggregator/RawFlightQuery.scala
+++ src/main/scala/com/flight/aggregator/RawFlightQuery.scala
@@ -29,9 +29,11 @@ object RawFlightQuery extends LazyLogging {
   implicit val formats: DefaultFormats.type = DefaultFormats
   val FULL_RECORD_LENGTH = 18
 
-  def validate(item:JValue): Boolean = {
+  // flight records periodically send an undocumented field that we still track but sometimes it is missing
+  // this will validate wheter or not field exists based on length
+  def valid(item:JValue): Boolean = {
     if (item.children.length < FULL_RECORD_LENGTH) {
-      logger.warn("Record received with invalid length: {}", item)
+      logger.warn("Record received with invalid length: {}. Replacing with empty value", item.children.length)
       false
     } else true
   }
@@ -41,13 +43,17 @@ object RawFlightQuery extends LazyLogging {
     val time = System.currentTimeMillis()
     val elements = (tree \\ "states").children
     val states = elements.flatMap(_.children)
-      .filter(validate)
-                         .map(item  => RawFlightRecord(item(0).extract[String],
-                           Option(item(1).extract[String].trim), item(2).extract[String], Option(item(3).extract[Int]),
-                           item(4).extract[Int], Option(item(5).extract[Double]), Option(item(6).extract[Double]),
-                           Option(item(7).extract[Double]), item(8).extract[Boolean], Option(item(9).extract[Double]), Option(item(10).extract[Double]),
-                           Option(item(11).extract[Double]), Option(item(12).extract[List[Int]].mkString("|")), Option(item(13).extract[Double]),
-                           Option(item(14).extract[String]), item(15).extract[Boolean], item(16).extract[Int], item(17).extract[Int]))
+                       .map(item  => {
+
+                         val missingUnknownField = if (!valid(item)) 0 else item(FULL_RECORD_LENGTH - 1).extract[Int]
+                         RawFlightRecord(item(0).extract[String],
+                         Option(item(1).extract[String].trim), item(2).extract[String], Option(item(3).extract[Int]),
+                         item(4).extract[Int], Option(item(5).extract[Double]), Option(item(6).extract[Double]),
+                         Option(item(7).extract[Double]), item(8).extract[Boolean], Option(item(9).extract[Double]), Option(item(10).extract[Double]),
+                         Option(item(11).extract[Double]), Option(item(12).extract[List[Int]].mkString("|")), Option(item(13).extract[Double]),
+                         Option(item(14).extract[String]), item(15).extract[Boolean], item(16).extract[Int],missingUnknownField)
+
+                       })
 
     return RawFlightQuery(time,states)
   }
